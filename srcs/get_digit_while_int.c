b2332d1160d5bc1924f4e582e1dd628c1134dc70
rstrawbe <kon.razinkin@yandex.ru>
2020-08-02 05:28:44 +0300

    add prec_init

--- srcs/get_digit_while_int.c
+++ srcs/get_digit_while_int.c
@@ -12,21 +12,26 @@
 
 #include "ft_printf.h"
 
-char	*get_digit_while_int(char *str, int *dest, int *spec_len)
+char	*get_digit_while_int(char *str, int *dest, t_spec *spec)
 {
 	int		res;
 
 	res = 0;
 	*dest = 0;
+	// if (spec->dot)
+	// 	spec->prec_init = '1';
+	// else if (is_digit(str + 1))
+	// 	spec->width_init = '1';
+
 	if (*str == '0')
 	{
-		*spec_len += 1;
+		spec->spec_len += 1;
 		return (str + 1);
 	}
 	while (is_digit(str))
 	{
 		res = (res * 10) + (*str++ - 48);
-		*spec_len += 1;
+		spec->spec_len += 1;
 	}
 	*dest = res;
 	return (str);
